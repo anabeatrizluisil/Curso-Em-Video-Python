150fa8bcd1d23030c1cbe52acca48e2524804106
Ana Silva <42662057+anabeatrizluisil@users.noreply.github.com>
2023-01-09 08:42:20 -0300

    Exercicio 4

---
+++ ex004.py
@@ -0,0 +1,13 @@
+# Desafio 04 - Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+
+algo = input('Digite algo: ')
+
+print('O tipo primitivo desse valor é {}'.format(type(algo)))
+print('Só tem espaços? {}'.format(algo.isspace()))
+print('É um número? {}'.format(algo.isnumeric()))
+print('É alfabético? {}'.format(algo.isalpha()))
+print('É alfanumérico? {}'.format(algo.isalnum()))
+print('Está em maiúsculas? {}'.format(algo.isupper()))
+print('Está em minúsculas? {}'.format(algo.islower()))
+print('Está capitalizada? {}'.format(algo.istitle()))
+
